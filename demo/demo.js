ee1199525a7fbbc2404bf8a93180c75fff74a032
Andrew Blowe <ablowe@barquin.com>
2018-05-31 16:32:38 -0400

    added demo data and started logic for building the chart

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,12 @@
 import ReactDOM from 'react-dom'
 import ScatterLine from '../src/ScatterLine'
+import demoData from './demoData.json'
 
-ReactDOM.render(<ScatterLine />, document.getElementById('demo-chart'))
+ReactDOM.render(
+  <ScatterLine 
+    data={demoData}
+    width={300}
+    height={200}
+  />,
+  document.getElementById('demo-chart')
+)
--- src/ScatterLine.js
+++ src/ScatterLine.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import d3 from 'd3'
 
-export default function({ className, data, nameAccessor, keyAccessor }) {
+export default function ScatterLine ({ className, data, width, height, defaultRadius, focusRadius, nameAccessor, valueAccessor }) {
+  const gutter = defaultRadius
+  const circleY = height / 2
+  const xDomain = [d3.min(data, valueAccessor), d3.max(data, valueAccessor)]
+  const xRange = width - (2 * gutter)
+  const xScale = d3.scale.linear.domain(xDomain).range(xRange)
+  const svgCircles = generateSvgCircles(data, xScale, defaultRadius, focusRadius, valueAccessor)
   return (
-    <div className="react-scatter-line">Hi</div>
+    <svg className={`react-scatter-line ${className || ''}`} width={width} height={height}>
+    
+    </svg>
   )
+}
+
+ScatterLine.defaultProps = {
+  width: 400,
+  height: 200,
+  defaultRadius: 5,
+  focusRadius: 20,
+  nameAccessor: (d) => d.name,
+  valueAccessor: (d) => d.value
+}
+
+function generateSvgCircles(data, xScale, yPosition, defaultRadius, focusRadius, valueAccessor) {
+  const circles = data.map((d) => {
+    let radius = defaultRadius
+    if(d.is_focus) radius = focusRadius
+
+    return (
+      <circle cx={xScale(valueAccessor(d))} cy={yPosition} r={radius} />
+    )
+  })
+  return circles
 }
